d49005524c64c8ff945de6a7a2933833e529dfb3
DarthManto <31321491+DarthManto@users.noreply.github.com>
2017-10-17 11:47:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,27 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `.`
+    if (message.substring(0, 1) == '.') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // .ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // .hello
+			case 'hello':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Hello! <@USER_ID>'
   	}
 });
 
