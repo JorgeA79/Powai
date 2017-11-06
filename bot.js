e9684a3e905d9a62c34fe238305dd64976a544fa
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2017-11-06 02:12:25 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setGame('xd')
+    client.user.setStatus(idle);
 });
 
 var commands = {
