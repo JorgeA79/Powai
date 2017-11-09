420c24896d8640f6e0a9edf7cc9c4e953c755a57
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2017-11-08 21:29:12 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready',() => {
-	console.log('I\'m Online\nI\'m Online');
+	 client.user.setPresence({game: {name: "with my code", type: 0}});
 });
 
 var prefix = "j."
