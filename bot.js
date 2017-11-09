46ef1a5e046d9ef94f62be7d21206db9eee98830
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2017-11-08 19:19:23 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,10 +17,16 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'hello')) {
 	
-		 message.channel.send(`Hello! :wave:`);
+		 message.channel.send(`Hello ${message.author.username}! :wave:`);
 	}
 });
-
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'help')) {
+	
+		 message.channel.send(`You need a little help with the commands:`);
+	}
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
