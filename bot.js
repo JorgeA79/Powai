7e9514d2bced7b440ce67c6e8818466e003fe900
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2017-11-08 19:17:04 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,13 @@ client.on('message', message => {
 		 message.channel.send(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``);
 	}
 });
-
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'hello')) {
+	
+		 message.channel.send(`Hello! :wave:`);
+	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
