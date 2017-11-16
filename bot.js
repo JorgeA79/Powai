0a39c47c13e21219cde11996ce83c2e07edb08d3
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2017-11-15 18:20:31 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'help')) {
 	
-		 message.author.send(`${message.author.username} you need a little help with the commands, there you go:\n **j.hello**\n **j.dbs**\n **j.ssj**\n **j.xd**\n **j.wait**\n **j.invite**\n **j.ping**`);
+		 message.author.send(`${message.author.username} you need a little help with the commands, there you go:\n **j.hello**\n **j.dbs**\n **j.ssj**\n **j.dex help**\n **j.xd**\n **j.wait**\n **j.invite**\n **j.ping**`);
 	 message.channel.send(`${message.author.username} message sent :mailbox:`)
 	}
 });
