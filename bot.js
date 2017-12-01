b5e145d2dc38c84a3068de157f1263aa64019c85
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2017-11-30 18:11:48 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,7 +132,7 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'dex list')) {
 	
-		 message.channel.send(`${message.author.username} here's the dex. \n**-Forshadic #1**\n **-Psychind #2**\n **-Foligsnow #3**\n**-Kascal #4**\n **-Skaloogan #5**\n **-Kingasrush #6**\n**-Pterark #41**\n **-Pteravolt #42**\n**-Parttiblow #61**\n **-Lattishine #62**\n**-Toxitrik #75**\n**-Ghoulantern #251**`);
+		 message.channel.send(`${message.author.username} here's the dex. \n**-Forshadic #1**\n **-Psychind #2**\n **-Foligsnow #3**\n**-Kascal #4**\n **-Skaloogan #5**\n **-Kingasrush #6**\n**-Frigipup #26**\n**-Pterark #41**\n **-Pteravolt #42**\n**-Parttiblow #61**\n **-Lattishine #62**\n**-Abandound #63**\n**-Toxitrik #75**\n**-Ghoulantern #251**`);
 	}
 });
 //1
