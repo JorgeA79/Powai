a7bc4cdc44776ac7562fde490f969c49fd80222a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2017-11-30 18:46:48 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,7 +132,7 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'dex list')) {
 	
-		 message.channel.send(`${message.author.username} here's the dex. \n**-Forshadic #1**\n **-Psychind #2**\n **-Foligsnow #3**\n**-Kascal #4**\n **-Skaloogan #5**\n **-Kingasrush #6**\n**-Frigipup #26**\n**-Pterark #41**\n **-Pteravolt #42**\n**-Parttiblow #61**\n **-Lattishine #62**\n**-Abandound #63**\n**-Toxitrik #75**\n**-Ghoulantern #251**`);
+		 message.channel.send(`${message.author.username} here's the dex. \n**-Forshadic #1**\n **-Psychind #2**\n **-Foligsnow #3**\n**-Kascal #4**\n **-Skaloogan #5**\n **-Kingasrush #6**\n**-Frigipup #26**\n**-Gargale #38**\n**-Pterark #41**\n **-Pteravolt #42**\n**-Parttiblow #61**\n **-Lattishine #62**\n**-Abandound #63**\n **-Mutterfall #64**\n**-Toxitrik #75**\n**-Ghoulantern #251**`);
 	}
 });
 //1
@@ -261,6 +261,27 @@ client.on('message', message => {
   message.channel.send({embed});
 	}
 });
+//38
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'dex gargale')) {
+	
+		const embed = new Discord.RichEmbed()
+  .setTitle("Gargale #38")
+  .setAuthor("ChaoticK-9", "https://cdn.discordapp.com/avatars/318162956510560257/a26b3e8586cfdff7dae799a193f37100.jpg")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x7401DF)
+  .setDescription("During the night it watches over the people in the city on a high ledge of a building scaring away the ghosts and spirits. During the day it goes to sleep sitting still not moving an inch acting like a statue.")
+  .setImage("https://cdn.discordapp.com/attachments/372491249221107714/385919379969277954/unknown.png")
+.addField("Types:",
+    "Rock/Dragon.")
+		.setFooter("Description by ChaoticK-9.")
+  .setTimestamp()
+  message.channel.send({embed});
+	}
+});
 //41
 client.on('message', message => {
 	if (message.author === client.user) return;
@@ -303,6 +324,7 @@ client.on('message', message => {
   message.channel.send({embed});
 	}
 });
+
 //61
 client.on('message', message => {
 	if (message.author === client.user) return;
@@ -361,6 +383,27 @@ client.on('message', message => {
   .setImage("https://cdn.discordapp.com/attachments/372491249221107714/385921094273597460/unknown.png")
 .addField("Types:",
     "Dark.")
+		.setFooter("Description by ChaoticK-9.")
+  .setTimestamp()
+  message.channel.send({embed});
+	}
+});
+//64
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'dex mutterfall')) {
+	
+		const embed = new Discord.RichEmbed()
+  .setTitle("Mutterfall #64")
+  .setAuthor("ChaoticK-9", "https://cdn.discordapp.com/avatars/318162956510560257/a26b3e8586cfdff7dae799a193f37100.jpg")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x7401DF)
+  .setDescription("Mutterfall evolved from Abandound by being introduced to a Water Stone. It is know that Mutterfall will often not battle when told to by its trainer.")
+  .setImage("https://cdn.discordapp.com/attachments/372491249221107714/385919309060374528/unknown.png")
+.addField("Types:",
+    "Water/Dark.")
 		.setFooter("Description by ChaoticK-9.")
   .setTimestamp()
   message.channel.send({embed});
