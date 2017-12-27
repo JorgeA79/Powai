e013b28ba88fb8a58bdbfad538987d712b97648a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2017-12-27 00:45:35 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,22 +46,7 @@ client.on('message', message => {
 		
 	}
 });
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith('welp even if u dont want ill do it')) {
-	
-	
-	 message.channel.send(`kk xd`);	
-	}
-});
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith('JorgeBot u ready for this?')) {
-	
-	 message.channel.send(`Hell na`);
-		
-	}
-});
+
 client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'xd')) {
